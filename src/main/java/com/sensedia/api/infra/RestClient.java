b30965809cb031b7df724e07eb2b16a7e24ddc60
Braz <anderson.braz@sensedia.com>
2017-07-03 15:00:25 -0300

    Interface fix

--- src/main/java/com/sensedia/api/infra/RestClient.java
+++ src/main/java/com/sensedia/api/infra/RestClient.java
@@ -33,8 +33,8 @@ public class RestClient {
 			return value;
 		}
 
-		public static Offset of(Integer value) {
-			return new Offset(value);
+		public static Limit of(Integer value) {
+			return new Limit(value);
 		}
 	}
 
